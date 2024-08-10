d5be3a8272fa8cfd3f68008b3ee088ff3ad6a187
Anthony Wilson <a02276199@usu.edu>
2024-08-09 20:07:50 -0600

    Updated password tests to get full coverage

--- RegularExpressions/src/test/kotlin/TestPassword.kt
+++ RegularExpressions/src/test/kotlin/TestPassword.kt
@@ -14,6 +14,11 @@ class TestPassword {
         assertTrue(verifier.verify("aaaaH!aaH!aa", initializer.createInitialState("password")!!))
         assertTrue(verifier.verify("1234567*9J", initializer.createInitialState("password")!!))
         assertTrue(verifier.verify("asdpoihj;loij@aH", initializer.createInitialState("password")!!))
+        assertTrue(verifier.verify("AAA1a!asd", initializer.createInitialState("password")!!))
+        assertTrue(verifier.verify("aA!a1aA!a1", initializer.createInitialState("password")!!))
+        assertTrue(verifier.verify("aAa123#Aa", initializer.createInitialState("password")!!))
+        assertTrue(verifier.verify("12abAB12@s", initializer.createInitialState("password")!!))
+        assertTrue(verifier.verify("12abAB12@1", initializer.createInitialState("password")!!))
     }
 
     @Test
@@ -21,9 +26,20 @@ class TestPassword {
         assertFalse(verifier.verify(" ", initializer.createInitialState("password")!!))
         assertFalse(verifier.verify("a", initializer.createInitialState("password")!!))
         assertFalse(verifier.verify("a1", initializer.createInitialState("password")!!))
-        assertFalse(verifier.verify("a1!", initializer.createInitialState("password")!!))
+        assertFalse(verifier.verify("1a", initializer.createInitialState("password")!!))
+        assertFalse(verifier.verify("a1!a1!a!", initializer.createInitialState("password")!!))
+        assertFalse(verifier.verify("1!a1!a1!", initializer.createInitialState("password")!!))
+        assertFalse(verifier.verify("!a1!a1!a", initializer.createInitialState("password")!!))
         assertFalse(verifier.verify("aaaaaa1!", initializer.createInitialState("password")!!))
         assertFalse(verifier.verify("Aaaaaa1", initializer.createInitialState("password")!!))
         assertFalse(verifier.verify("aA!a1", initializer.createInitialState("password")!!))
+        assertFalse(verifier.verify("Fals3", initializer.createInitialState("password")!!))
+        assertFalse(verifier.verify("aaaaaaaa{a", initializer.createInitialState("password")!!))
+        assertFalse(verifier.verify("aaaaaa1{a", initializer.createInitialState("password")!!))
+        assertFalse(verifier.verify("aaaaaaA{a", initializer.createInitialState("password")!!))
+        assertFalse(verifier.verify("{aaaaaa1A", initializer.createInitialState("password")!!))
+        assertFalse(verifier.verify("aaaaaa1\$A{", initializer.createInitialState("password")!!))
+        assertFalse(verifier.verify("aaaaaa1\${A", initializer.createInitialState("password")!!))
+        assertFalse(verifier.verify("aaaaaa\$A#1#{", initializer.createInitialState("password")!!))
     }
 }
